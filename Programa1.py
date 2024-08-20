a84423d08b14467621daeac7967d65975d300e3a
JustinMtz <supermeatboy14@gmail.com>
2024-08-20 10:34:30 -0600

    Update Programa1.py

--- Programa1.py
+++ Programa1.py
@@ -1,1 +1,14 @@
 
+def clasificar_cadena(cadena):
+    if cadena.isdigit():
+        return "Número entero"
+    elif cadena.isalpha():
+        return "Palabra"
+    elif any(char.isdigit() for char in cadena) and any(char.isalpha() for char in cadena):
+        return "Compuesta"
+    else:
+        return "Desconocida"
+
+cadena_usuario = input("Ingrese una cadena de caracteres: ")
+resultado = clasificar_cadena(cadena_usuario)
+print(f"La cadena ingresada es: {resultado}")
